b9e0c27a496ddce7245568ebc0dddd47eb178561
Milan Burmaja <mburmaja@groundlink.com>
2014-11-08 14:57:49 +0100

    Added slate configuration

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files=".gemrc	.gitignore_global .vimrc .git-prompt.sh .railsrc .bashrc .gitconfig .vim"    # list of files/folders to symlink in homedir
+files=".slate .gemrc	.gitignore_global .vimrc .git-prompt.sh .railsrc .bashrc .gitconfig .vim"    # list of files/folders to symlink in homedir
 
 ##########
 
